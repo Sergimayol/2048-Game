455e952f39e07988de6c3dd5eee1a0f1f1fd64da
Sergi Mayol <sergi2808@gmail.com>
2022-04-04 19:07:59 +0200

    updated

---
+++ main.js
@@ -0,0 +1,9 @@
+function createGrid() {
+    var container = document.getElementsByClassName('grid-container');
+    const n = 16;
+    for (let i = 0; i < n; i++) {
+        var row = document.createElement('div');
+        row.className = 'grid-cell';
+        container[0].appendChild(row);
+    }
+}
