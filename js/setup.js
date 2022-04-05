8a47b2cbabee11de348504d156da954ccf8e0424
Sergi Mayol <sergi2808@gmail.com>
2022-04-05 10:35:57 +0200

    updated

--- js/setup.js
+++ js/setup.js
@@ -8,3 +8,14 @@ function createGrid() {
     }
 }
 
+function createGridLayout() {
+    var container = document.getElementsByClassName('grid-container');
+    const n = 4;
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            var cell = document.createElement('div');
+            cell.className = 'grid-cell' + ' ' + i + j;
+            container[0].appendChild(cell);
+        }
+    }
+}
